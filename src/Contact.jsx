e2172707ba9e7bed1682f1e0f800d8a592b61e5a
DPopraw <danielbpoprawski@gmail.com>
2024-12-03 18:37:56 -0500

    Added contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,13 +1,84 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import { Container, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { InputGroup } from "react-bootstrap";
 
 export default function Contact() {
+        const [data, setData] = useState({
+                name: "",
+                email: "",
+                message: "",
+        });
 
-        return <>
-        <Header/>
-                <div className="hero">
-                        <h2> Contact: </h2>
-                        <h4> If you would like to contact me, please leave a message here with your name, email (or phone number) and what services I may provide for you and I will contact you as soon as possible</h4>
-                </div>
-                
-        </>
+        const handleChange = (e) => {
+                setData({
+                        ...data,
+                        [e.target.name]: e.target.value,
+                });
+        };
+
+        const handleSubmit = (e) => {
+                e.preventDefault();
+                console.log(data);
+                setData({
+                        name: "",
+                        email: "",
+                        message: "",
+                });
+        };
+
+        return (
+                <>
+                        <Header />
+                        <div className="hero">
+                                <h2> Contact: </h2>
+                                <h4>
+                                        If you would like to contact me, please leave a message here with your name,
+                                        email (or phone number) and what services I may provide for you and I will
+                                        contact you as soon as possible
+                                </h4>
+                                <Form onSubmit={handleSubmit} className="contact-form">
+                                        <InputGroup controlId="formName">
+                                                <InputGroup.Text>Name</InputGroup.Text>
+                                                <Form.Control
+                                                        type="text"
+                                                        name="name"
+                                                        value={data.name}
+                                                        onChange={handleChange}
+                                                        placeholder="John Doe"
+                                                />
+                                        </InputGroup>
+                                        <br />
+
+                                        <InputGroup controlId="formEmail">
+                                                <InputGroup.Text>Email</InputGroup.Text>
+                                                <Form.Control
+                                                        type="email"
+                                                        name="email"
+                                                        value={data.email}
+                                                        onChange={handleChange}
+                                                        placeholder="johndoe@youremail.net"
+                                                />
+                                        </InputGroup>
+                                        <br />
+
+                                        <InputGroup controlId="formMessage">
+                                                <Form.Control
+                                                        as="textarea"
+                                                        rows={3}
+                                                        name="message"
+                                                        value={data.message}
+                                                        onChange={handleChange}
+                                                        placeholder="Enter your message"
+                                                />
+                                        </InputGroup>
+                                        <br />
+
+                                        <Button variant="primary" type="submit">
+                                                Submit
+                                        </Button>
+                                </Form>
+                        </div>
+                </>
+        );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import Home from "./Home.jsx";
 import Contact from "./Contact.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
         <StrictMode>
-                <App />
+                <BrowserRouter>
+                        <Routes>
+                                <Route element={<Home />} path="/" />
+                                <Route element={<Contact />} path="/contact" />
+                        </Routes>
+                </BrowserRouter>
         </StrictMode>
 );
