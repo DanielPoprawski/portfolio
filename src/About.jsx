08548ab7daf90a09c518d4f5feca5d28945a2a62
DPopraw <danielbpoprawski@gmail.com>
2024-12-03 18:55:01 -0500

    Added basic About page

---
+++ src/About.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+
+export default function About() {
+        return (
+                <>
+                        <Header />
+                        <div className="hero">
+                                <p>
+                                        &emsp; My name is Daniel Poprawski. I just got out of the Army after 4 years of
+                                        service and I am pursuing a career as a front-end web developer. I have been
+                                        programming since I was a teenager when I first discovered HTML and CSS, and
+                                        later Javascript, my first programming language. After playing with Javscript I
+                                        moved on to Python and Java and then finally came back to Javascript after
+                                        discovering React. Programming is something that I have been passionate about
+                                        for as long as I remember and I would be very happy and honored to build your
+                                        website for you.
+                                </p>
+                        </div>
+                </>
+        );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./Home.jsx";
 import Contact from "./Contact.jsx";
+import About from "./About.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
                         <Routes>
                                 <Route element={<Home />} path="/" />
                                 <Route element={<Contact />} path="/contact" />
+                                <Route element={<About />} path="/about" />
                         </Routes>
                 </BrowserRouter>
         </StrictMode>
